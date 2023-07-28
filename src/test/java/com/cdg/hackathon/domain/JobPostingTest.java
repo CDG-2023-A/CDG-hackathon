8baec31c220e3cda970d72d3aa1a55ff411f6be9
wellbeing-dough <kdw990202@naver.com>
2023-07-28 19:52:05 +0900

    수정 삭제 테스트 코드

--- src/test/java/com/cdg/hackathon/domain/JobPostingTest.java
+++ src/test/java/com/cdg/hackathon/domain/JobPostingTest.java
@@ -1,6 +1,7 @@
 package com.cdg.hackathon.domain;
 
 import com.cdg.hackathon.dto.request.CreateJobPostingRequest;
+import com.cdg.hackathon.dto.request.UpdateJobPostingRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,40 @@ class JobPostingTest {
                         "techStack")
                 .containsExactlyInAnyOrder(
                         companyId,
+                        position,
+                        reward,
+                        content,
+                        techStack
+                );
+    }
+
+    @DisplayName("UpdateJobPostingRequest를 입력받아서 JobPosting을 수정한다.")
+    @Test
+    public void updateJobPosting() {
+        //given
+        JobPosting jobPosting = JobPosting.builder()
+                .companyId(1L)
+                .position("백엔드 주니어 개발자")
+                .reward(1000000)
+                .content("원티드랩에서 백엔드 주니어 개발자를 채용합니다.")
+                .techStack("springBoot")
+                .build();
+
+        //when
+        String position = "벡엔드 시니어 개발자";
+        Integer reward = 10000000;
+        String content = "원티드랩에서 벡엔드 시니어 개발자를 채용합니다.";
+        String techStack = "springBoot";
+        UpdateJobPostingRequest request = new UpdateJobPostingRequest(position, reward, content, techStack);
+        jobPosting.update(request);
+        //then
+
+        Assertions.assertThat(jobPosting).extracting(
+                        "position",
+                        "reward",
+                        "content",
+                        "techStack")
+                .containsExactlyInAnyOrder(
                         position,
                         reward,
                         content,
--- src/test/java/com/cdg/hackathon/service/JobPostingServiceTest.java
+++ src/test/java/com/cdg/hackathon/service/JobPostingServiceTest.java
@@ -2,6 +2,7 @@ package com.cdg.hackathon.service;
 
 import com.cdg.hackathon.domain.JobPosting;
 import com.cdg.hackathon.dto.request.CreateJobPostingRequest;
+import com.cdg.hackathon.dto.request.UpdateJobPostingRequest;
 import com.cdg.hackathon.repository.JobPostingRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -54,4 +59,65 @@ class JobPostingServiceTest {
                         techStack
                 );
     }
+
+    @DisplayName("채용공고를 수정해서 저장한다.")
+    @Test
+    public void updateJobPosting() {
+        // given
+        JobPosting jobPosting = JobPosting.builder()
+                .companyId(1L)
+                .position("백엔드 주니어 개발자")
+                .reward(1000000)
+                .content("원티드랩에서 백엔드 주니어 개발자를 채용합니다.")
+                .techStack("springBoot")
+                .build();
+        JobPosting createdJobPosting = jobPostingRepository.save(jobPosting);
+
+        String position = "벡엔드 시니어 개발자";
+        Integer reward = 10000000;
+        String content = "원티드랩에서 벡엔드 시니어 개발자를 채용합니다.";
+        String techStack = "springBoot";
+
+        //when
+        UpdateJobPostingRequest request = new UpdateJobPostingRequest(position, reward, content, techStack);
+        jobPostingService.updateJobPosting(jobPosting.getId(), request);
+        JobPosting findJobPosting = jobPostingRepository.findById(createdJobPosting.getId()).orElseThrow();
+
+        //then
+        Assertions.assertThat(findJobPosting).extracting(
+                        "position",
+                        "reward",
+                        "content",
+                        "techStack")
+                .containsExactlyInAnyOrder(
+                        position,
+                        reward,
+                        content,
+                        techStack
+                );
+
+    }
+
+    @DisplayName("채용공고를 삭제한다.")
+    @Test
+    public void deletePostingJob() {
+        //given
+        JobPosting jobPosting = JobPosting.builder()
+                .companyId(1L)
+                .position("백엔드 주니어 개발자")
+                .reward(1000000)
+                .content("원티드랩에서 백엔드 주니어 개발자를 채용합니다.")
+                .techStack("springBoot")
+                .build();
+        JobPosting createdJobPosting = jobPostingRepository.save(jobPosting);
+
+        //when
+        jobPostingService.deleteJobPosting(createdJobPosting.getId());
+
+        //then
+        assertThrows(NoSuchElementException.class, () -> jobPostingRepository.findById(createdJobPosting.getId()).get());
+
+    }
+
+
 }
